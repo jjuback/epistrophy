f850c4da82b9d3c8fd6663fa0a763aa5a7871e97
John Juback <johnjuback@gmail.com>
2024-01-09 14:07:07 -0500

    Initial support for MediaSession API

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import Container from 'react-bootstrap/Container';
 
 const App = () => {
     const [album, setAlbum] = useState();
+    const [artist, setArtist] = useState('');
     const [current, setCurrent] = useState();
     const [scrollY, setScrollY] = useState(0);
-    const selectAlbum = (data) => { setAlbum(data); setScrollY(window.scrollY); }
+    const selectAlbum = (data, name) => { setAlbum(data); setArtist(name); setScrollY(window.scrollY); }
     const goBack = () => setAlbum(undefined);
 
     useEffect(() => {
@@ -28,7 +29,7 @@ const App = () => {
           return (
             <Container className="p-3">
               <div className="App">
-                <Album {...album} goBack={goBack} />
+                <Album {...album} Artist={artist} goBack={goBack} />
               </div>
             </Container>
           )
--- src/Artists.js
+++ src/Artists.js
@@ -17,17 +17,17 @@ export const Artists = (props) => {
   return (
     <>
       <Accordion defaultActiveKey={props.current}>
-        {cdVault.map((data, key) => {
+        {cdVault.map((artist, key) => {
             return (
                 <Accordion.Item key={key} eventKey={key}>
-                    <Accordion.Header>{data.Name}</Accordion.Header>
+                    <Accordion.Header>{artist.Name}</Accordion.Header>
                     <Accordion.Body>
                         <Container>
                           <Row className="flex-wrap">
-                          {data.Albums.map((data, index) => {
+                          {artist.Albums.map((data, index) => {
                             return (
                               <Col key={index} className="col-auto figure text-center">
-                                  <Figure className="align-items-center" onClick={() => { props.setCurrent(key); props.selectAlbum(data) }}>
+                                  <Figure className="align-items-center" onClick={() => { props.setCurrent(key); props.selectAlbum(data, artist.Name) }}>
                                     <Figure.Image className="cover-thumbnail" src={makeUrl(data.Cover)} />
                                     <Figure.Caption className="cover-caption">
                                       {data.Title}
