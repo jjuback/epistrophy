bda598b821a1ecd81c82bb5a3988bf3f80e30f14
John Juback <johnjuback@gmail.com>
2024-04-02 17:39:23 -0400

    Experiment with onEnded

--- src/Album.js
+++ src/Album.js
@@ -24,8 +24,8 @@ export const Album = (props) => {
                  ]}
                  autoPlay={trackNo > 0}
                  onEnded={() => {
-                    if (trackNo + 1 < props.Tracks.length) {
-                        setTrackNo(trackNo + 1);
+                    if (trackNo + 3 < props.Tracks.length) {
+                        setTrackNo(trackNo + 3);
                     }
                  }}
                 >
